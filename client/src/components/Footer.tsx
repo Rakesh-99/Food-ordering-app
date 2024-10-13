f2fedab3efa08c0eb29ac6de23853f542d4e7301
Rakesh Kumar Parida <98577842+Rakesh-99@users.noreply.github.com>
2024-10-13 23:23:14 +0530

    complete footer UI

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,8 +1,19 @@
+import { Link } from "react-router-dom";
+import { FaGithub, FaTwitter } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa6";
+
 const Footer = () => {
   return (
     <>
-      <div className="">
-        <h1>Footer</h1>
+      <div className="w-full text-center h-16 border-t flex-col md:flex-row lg:flex-row flex justify-around items-center py-2">
+        <p className="">Developed and designed with love by <Link to='' className="text-violet-500 font-[500]">@Rakesh Kumar Parida</Link></p>
+
+
+        <div className="flex  gap-10 py-5">
+          <span className="border-2 cursor-pointer rounded-full px-2 py-2 shadow "><FaGithub size={21} className="hover:text-violet-700 transition-all" /></span>
+          <span className="border-2 cursor-pointer rounded-full px-2 py-2 shadow "><FaTwitter size={21} className="hover:text-violet-700 transition-all" /></span>
+          <span className="border-2 cursor-pointer rounded-full px-2 py-2 shadow "><FaLinkedin size={21} className="hover:text-violet-700 transition-all" /></span>
+        </div>
       </div>
     </>
   );
