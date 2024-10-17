1a9d9e547a206c85fcd3f20371a7789fb79d7de2
Rakesh Kumar Parida <98577842+Rakesh-99@users.noreply.github.com>
2024-10-18 02:15:26 +0530

    add extra condition

--- client/src/zod/schema-restaurant/restaurant.ts
+++ client/src/zod/schema-restaurant/restaurant.ts
@@ -10,7 +10,7 @@ export const restaurantSchema = z.object({
 
     restaurant_city: z.string().min(2, 'City must be at least 2 char! ').max(10, 'City name can not exceed 10 char!'),
 
-    delivery_time: z.number().max(60, "Delivery time cannot exceed 60 minutes!").min(1, "Delivery time must be at least 1 minute!"),
+    delivery_time: z.number().max(60, "Delivery time cannot exceed 60 minutes!").min(1, "Delivery time must be at least 1 minute!").min(0, "Delivery time can not be set to negetive time"),
 
     // cuisines: z.string().min(3, 'Cuisine can not be less than 3 char').max(20, 'Cuisines can not exceed 20 char!'),
 
@@ -19,7 +19,7 @@ export const restaurantSchema = z.object({
     description: z.string().min(10, 'Description can not be less than 10 char!').max(25, 'Description can not exceed 25 char!'),
 
     restaurant_country: z.string().min(2, 'Country name can not be less than 3 char!').max(10, 'Country name can not exceed 10 char!'),
-    
+
     restaurant_image: z.instanceof(File).optional().refine((file) => file?.size !== 0, { message: "Image is required!" })
 });
 
