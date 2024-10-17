bfd777220ef4dde143b9fe2fbad1899216ef0986
Rakesh Kumar Parida <98577842+Rakesh-99@users.noreply.github.com>
2024-10-18 02:15:46 +0530

    add more types

--- client/src/constants/dataTypes.ts
+++ client/src/constants/dataTypes.ts
@@ -1,3 +1,18 @@
+import { IconType } from "react-icons";
+
 export type BadgeType = {
     foodName: string
+}
+
+
+export interface NavLinksTypes {
+    path: string,
+    label: string,
+    icon: IconType
+}
+
+export type DropDownTypes = {
+    showStatusBar: boolean,
+    showActivityBar: boolean,
+    showPanel: boolean
 }
