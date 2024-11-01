116bd36840687efdf3e9d8611bf4693d02a7a5c3
Rakesh-99 <rakeshkumarparida424@gmail.com>
2024-11-01 10:39:18 +0530

    features : auth routes including login,verify-email,logout,forget and reset password

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,15 +1,18 @@
 import express from 'express';
 const userRouters = express.Router();
-import { userSignup, userLogin } from "../controller/user.controller";
+import { userSignup, userLogin, verifyEmail, logoutUser, forgetPassword, resetPassword } from "../controller/user.controller";
 
 
 
 
 
-userRouters.post('/signup', userSignup)
+userRouters
+    .post('/signup', userSignup)
     .post('/login', userLogin)
-
-
+    .post('/verify-email', verifyEmail)
+    .post('/logout', logoutUser)
+    .post('/forget-password', forgetPassword)
+    .post('/reset-passsword/:passwordtoken', resetPassword)
 
 
 export default userRouters;
