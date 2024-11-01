96525d426cd310184bbd789320eaf205289b7c4b
Rakesh-99 <rakeshkumarparida424@gmail.com>
2024-11-01 10:37:50 +0530

    implement nodemailer for sending reset password

--- server/config/sendEmails.ts
+++ server/config/sendEmails.ts
@@ -1,26 +1,43 @@
-import { NextFunction, Response } from 'express';
 import { transport } from './email.config';
-import { VERIFICATION_EMAIL_TEMPLATE } from './emailTemplate';
-import { ErrorHandler } from '../utils/ErrorHandler';
+import { VERIFICATION_EMAIL_TEMPLATE, PASSWORD_RESET_REQUEST_TEMPLATE } from './emailTemplate';
 
 
 
 
 
-// Send veriofication email : 
+
+// Send verification email : 
 export const sendVerificationMail = async (email: string, verificationToken: string) => {
     try {
         const sendMessage = await transport.sendMail({
             from: process.env.USER_EMAIL,
             to: email,
-            subject: "Verify Your Email",
+            subject: "Food Frenzy || Verify Your Email",
             html: VERIFICATION_EMAIL_TEMPLATE.replace("{verificationCode}", verificationToken)
         })
         console.log(`Message has been sent successfully ${sendMessage.messageId}`);
 
     } catch (error) {
-        // return next(new ErrorHandler(400, `The email could not deliver! , ${error}`));
         console.log(`The email could not deliver! , ${error}`);
+    }
+};
 
+
+// Send reset password token : 
+
+export const sendResetPasswordToken = async (email: string, token: string) => {
+
+    try {
+        const sendLink = transport.sendMail({
+            from: process.env.EMAIL_USER,
+            to: email,
+            subject: "Food Frenzy || Request for reset password",
+            html: PASSWORD_RESET_REQUEST_TEMPLATE.replace("{resetURL}", token)
+
+        });
+        console.log(`Password reset link has been sent successfully-  ${(await sendLink).messageId}`,);
+
+    } catch (error) {
+        console.log(`Could not send reset password link! : ${error}`);
     }
 }
