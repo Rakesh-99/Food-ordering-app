1a59648318ad16ff352b3dfd71ff2609e7170bc5
Rakesh-99 <rakeshkumarparida424@gmail.com>
2024-11-07 00:06:34 +0530

    improve overall functionalities of controllers

--- server/controller/user.controller.ts
+++ server/controller/user.controller.ts
@@ -3,14 +3,27 @@ import userModel from "../models/user.model";
 import bcrypt from 'bcrypt';
 import { ErrorHandler } from "../utils/ErrorHandler";
 import asyncErrorHandler from 'express-async-handler';
-import createJwtTokenAndCookies from "../utils/jwtTokenAndCookies";
+import createJwtToken from "../utils/jwtTokenAndCookies";
 import { sendVerificationMail } from "../config/sendEmails";
 import crypto from 'crypto';
 import dotenv from 'dotenv';
 dotenv.config();
 import { sendResetPasswordToken } from "../config/sendEmails";
+import { uploadOnCloudinary } from "../utils/cloudinary-setup/cloudinary";
+import { sendSuccessfullResetEmail } from "../config/sendEmails";
+import { unlink } from "fs/promises";
 
 
+
+
+interface IUserTypes {
+    fullname: string,
+    email: string,
+    password: string,
+    city: string,
+    address: string,
+    country: string
+}
 
 
 
@@ -21,7 +34,6 @@ export const userSignup = asyncErrorHandler(async (req: Request, res: Response, 
 
 
     // Check if the user is already exist in db : 
-
     const isUserExist = await userModel.findOne({ email });
     if (isUserExist) {
         return next(new ErrorHandler(400, "User is already exist!"));
@@ -30,66 +42,33 @@ export const userSignup = asyncErrorHandler(async (req: Request, res: Response, 
     // Hash the plain password coming from req body: 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // create a verification token : 
-
-    const verificationToken = Math.floor(100000 + Math.random() * 200000).toString();
+    // create a six digit verification code  : 
+    const verificationCode = Math.floor(100000 + Math.random() * 200000).toString();
 
     // create a new user : 
-
     const newUser = new userModel({
         fullname,
         email,
         contact,
         password: hashedPassword,
-        verificationToken,
-        verificationTokenExpiresAt: Date.now() + 1 * 60 * 1000  // 1 minute
+        verificationToken: verificationCode,
+        verificationTokenExpiresAt: new Date(Date.now() + 15 * 60 * 1000) // OTP will expire in 15 minutes 
     })
     await newUser.save();
 
-    // send the new user data in response without password : 
-    // convert the mongoose document in plain object : 
+    // convert the mongoose document in plain object, then send the new user data in response without password : 
     const { password: _, ...rest } = newUser.toObject();
 
 
+    // Create jwt token and set cookies : 
+    createJwtToken(res, newUser._id);
+
     // Send verification mail : 
-    await sendVerificationMail(newUser.email, verificationToken);
+    await sendVerificationMail(newUser.email, verificationCode);
 
     return res.status(200).json({ success: true, message: 'User has been registered', user: rest });
-})
-
-//User login : 
-export const userLogin = asyncErrorHandler(async (req: Request, res: Response, next: NextFunction): Promise<any> => {
-    const { email, password }: { email: string, password: string } = req.body;
-
-    // Check if the user is exist or not : 
-
-    const isUserExist = await userModel.findOne({ email });
-
-    if (!isUserExist) {
-        return next(new ErrorHandler(401, "Invalid username or passowrd!"));
-    }
-
-    // compare both password : 
-
-    const isPasswordMatch = await bcrypt.compare(password, isUserExist.password);
-
-    // Check if the passwords are matching or not : 
-
-    if (!isPasswordMatch) {
-        return next(new ErrorHandler(401, "Invalid email or password!"));
-    }
-
-    // Generate token : 
-    createJwtTokenAndCookies(res, isUserExist._id);
-    isUserExist.lastLogin = new Date();
-
-
-    // Send user data without pasword in response : 
-    const userWithoutPassword = isUserExist.toObject();
-    const { password: _, ...rest } = userWithoutPassword;
-
-    return res.status(200).json({ success: true, message: `Welcome back ${isUserExist.fullname}`, user: rest })
 });
+
 
 
 //Verify email : 
@@ -114,6 +93,45 @@ export const verifyEmail = asyncErrorHandler(async (req: Request, res: Response,
     return res.status(200).json({ success: true, message: `Hi ${user.fullname}, you have been verified` });
 
 })
+
+//User login : 
+export const userLogin = asyncErrorHandler(async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+    const { email, password }: { email: string, password: string } = req.body;
+
+    // Check if the user is exist or not : 
+
+    const isUserExist = await userModel.findOne({ email });
+
+    if (!isUserExist) {
+        return next(new ErrorHandler(401, "Invalid username or passowrd!"));
+    }
+
+    // compare both password : 
+
+    const isPasswordMatch = await bcrypt.compare(password, isUserExist.password);
+
+    // Check if the passwords are matching or not : 
+
+    if (!isPasswordMatch) {
+        return next(new ErrorHandler(401, "Invalid email or password!"));
+    }
+
+    // Password mateched, create the jwt token and set the cookie : 
+
+    createJwtToken(res, isUserExist._id);
+
+
+    // Send user data without pasword in response : 
+    const userWithoutPassword = isUserExist.toObject();
+    const { password: _, ...rest } = userWithoutPassword;
+
+    isUserExist.lastLogin = new Date(Date.now());
+
+    await isUserExist.save();
+
+    return res.status(200).json({ success: true, message: `Welcome back ${isUserExist.fullname}`, user: rest })
+});
+
 
 
 //Logout user : 
@@ -141,16 +159,18 @@ export const forgetPassword = asyncErrorHandler(async (req: Request, res: Respon
     // Generate reset password token : 
 
     const resetToken = crypto.randomBytes(20).toString("hex");
-    const resetTokenExpire = Date.now() + 10 * 60 * 1000   // token will be expired after 10 minuites
+    const resetTokenExpire = new Date(Date.now() + 10 * 60 * 1000)// token will be expired after 10 minuites
 
 
-    user.resetPasswordToken = resetToken;
-    user.resetPasswordTokenExpiresAt = new Date(resetTokenExpire);
+    const updateUser = await userModel.findByIdAndUpdate(
+        user._id,
+        {
+            resetPasswordToken: resetToken,
+            resetPasswordTokenExpiresAt: resetTokenExpire
+        }
+    );
+    await updateUser?.save();
 
-
-    // save the updated data in the db : 
-
-    await user.save();
     // send reset password token : 
 
     await sendResetPasswordToken(user.email, `${process.env.CLIENT_BASE_URL}/reset-password/${resetToken}`)
@@ -170,19 +190,93 @@ export const resetPassword = asyncErrorHandler(async (req: Request, res: Respons
 
     const user = await userModel.findOne({ resetPasswordToken: passwordtoken });
 
-    if (!user) {
+    if (!user || user.resetPasswordTokenExpiresAt && user.resetPasswordTokenExpiresAt < new Date(Date.now())) {
         return next(new ErrorHandler(401, "Invalid link or link has been expired!"));
     }
 
-    // Convert the plain password to hashed format : 
+    // If the token matched with Database token then create a password and convert it into hash format: 
+
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // if the both token match : 
+    // update in db 
 
-    await userModel.findByIdAndUpdate(user._id, { password: hashedPassword });
-    await user.save();
+    const updateUser = await userModel.findByIdAndUpdate(
+        user._id,
+        {
+            password: hashedPassword,
+            resetPasswordToken: undefined,
+            resetPasswordTokenExpiresAt: undefined
+        }
+    );
+    await updateUser?.save();
+
+    //Send successfull reset email:
+
+    await sendSuccessfullResetEmail(user.email);
 
     return res.status(200).json({ success: true, message: `Hi ${user.fullname} , Your password has been updated` });
 });
 
+
+// Check auth : 
+export const checkAuth = asyncErrorHandler(async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+
+    const user = await userModel.findById({ _id: req.userId }).select("-password");
+
+    if (!user) {
+        return next(new ErrorHandler(401, "User not found !"));
+    }
+
+    return res.status(200).json({ success: true, message: "Your profile has been fetched", user });
+});
+
+
+
+
+// Update user profile: 
+export const updateUserProfile = asyncErrorHandler(async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+
+    const userId = req.userId;
+    const user = await userModel.findById({ _id: userId }).select("-password");
+
+    if (!user) {
+        return next(new ErrorHandler(400, "User not found!"));
+    }
+
+    const userImage = req.file;
+    const { fullname, email, password, city, address, country }: Partial<IUserTypes> = req.body;
+
+    // if file is there, upload it on cloudinary : 
+    const cloudinaryImgURL = await uploadOnCloudinary(userImage?.path);
+
+
+    let hashedPassword;
+    // If password available convert it into hash : 
+    if (password) {
+        hashedPassword = await bcrypt.hash(password, 10);
+    }
+
+    // Update the userDetails : 
+    const updateUserChanges = await userModel.findByIdAndUpdate(
+        user._id,
+        {
+            profilePicture: cloudinaryImgURL || user.profilePicture,
+            fullname: fullname || user.fullname,
+            email: email || user.email,
+            password: hashedPassword || user.password,
+            city: city || user.city,
+            address: address || user.address,
+            country: country || user.country
+        },
+        { new: true }
+    );
+    await updateUserChanges?.save();
+
+    // Delete the file from the folder after image has been pushed to the cloudinary :
+    if (updateUserChanges && userImage) {
+        unlink(userImage.path)
+    }
+
+    return res.status(200).json({ success: true, message: "User profile has been updated", user: updateUserChanges })
+})
