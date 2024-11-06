1c4f149b8e8ecd76c4e311cee1edd80fab9ecce3
Rakesh-99 <rakeshkumarparida424@gmail.com>
2024-11-06 23:57:53 +0530

    fetaures : implement check-auth and update-user route

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,18 +1,21 @@
 import express from 'express';
 const userRouters = express.Router();
-import { userSignup, userLogin, verifyEmail, logoutUser, forgetPassword, resetPassword } from "../controller/user.controller";
-
-
+import { userSignup, userLogin, verifyEmail, logoutUser, forgetPassword, resetPassword, updateUserProfile, checkAuth } from "../controller/user.controller";
+import { verifyUserByJwt } from '../middleware/verifyJwtToken';
+import uploadFile from '../middleware/multer.config';
 
 
 
 userRouters
+    .get('/check-auth', verifyUserByJwt, checkAuth)
     .post('/signup', userSignup)
     .post('/login', userLogin)
     .post('/verify-email', verifyEmail)
     .post('/logout', logoutUser)
     .post('/forget-password', forgetPassword)
     .post('/reset-passsword/:passwordtoken', resetPassword)
+    .put('/update-user', verifyUserByJwt, uploadFile.single("profilePicture"), updateUserProfile)
+
 
 
 export default userRouters;
