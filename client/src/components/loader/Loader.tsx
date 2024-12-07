ab9656711daae9ab7825ae71e99a06b8a5b5e625
Rakesh-99 <rakeshkumarparida424@gmail.com>
2024-12-07 22:37:35 +0530

    add loader

---
+++ client/src/components/loader/Loader.tsx
@@ -0,0 +1,14 @@
+import '../../styles/loader.css';
+const Loader = () => {
+    return (
+        <>
+            <div className="flex justify-center items-center min-h-96">
+                <div className="loader">
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Loader
