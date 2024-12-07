d7ad7e831b3f4f6043ebf88e289233b7a9c7c495
Rakesh-99 <rakeshkumarparida424@gmail.com>
2024-12-07 22:19:19 +0530

    configure cors

--- server/index.ts
+++ server/index.ts
@@ -15,8 +15,19 @@ import orderRouter from './routes/order.routes';
 
 
 
+
+// Cors configurations : 
+const corsConfig = {
+    origin: `http://localhost:5173`,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-type', 'Authorization']
+}
+
+
+
 // Middlewares : 
-app.use(cors());
+app.use(cors(corsConfig));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
