6f9d0f725a67ecbdbb28f1ed4d7552aec33e7560
Rakesh-99 <rakeshkumarparida424@gmail.com>
2024-12-07 22:47:12 +0530

    fix type in zod file validation

--- client/src/zod/schema-restaurant/restaurant.ts
+++ client/src/zod/schema-restaurant/restaurant.ts
@@ -6,21 +6,24 @@ import { z } from 'zod';
 export const restaurantSchema = z.object({
 
 
-    restaurant_name: z.string().max(20, "Restaurant name can not exceed 20 char!").min(4, "Restaurant name can not be less than 4 char!"),
+    restaurantName: z.string().max(20, "Restaurant name can not exceed 20 char!").min(4, "Restaurant name can not be less than 4 char!"),
 
-    restaurant_city: z.string().min(2, 'City must be at least 2 char! ').max(10, 'City name can not exceed 10 char!'),
+    city: z.string().min(2, 'City must be at least 2 char! ').max(10, 'City name can not exceed 10 char!'),
 
-    delivery_time: z.number().max(60, "Delivery time cannot exceed 60 minutes!").min(1, "Delivery time must be at least 1 minute!").min(0, "Delivery time can not be set to negetive time"),
+    deliveryTime: z.number().max(60, "Delivery time cannot exceed 60 minutes!").min(1, "Delivery time must be at least 1 minute!").min(0, "Delivery time can not be set to negetive time"),
 
     // cuisines: z.string().min(3, 'Cuisine can not be less than 3 char').max(20, 'Cuisines can not exceed 20 char!'),
 
     cuisines: z.array(z.string()),
 
-    description: z.string().min(10, 'Description can not be less than 10 char!').max(25, 'Description can not exceed 25 char!'),
+    description: z.string().min(20, 'Description can not be less than 10 char!').max(50, 'Description can not exceed 50 char!'),
 
-    restaurant_country: z.string().min(2, 'Country name can not be less than 3 char!').max(10, 'Country name can not exceed 10 char!'),
+    country: z.string().min(2, 'Country name can not be less than 3 char!').max(10, 'Country name can not exceed 10 char!'),
 
-    restaurant_image: z.instanceof(File).optional().refine((file) => file?.size !== 0, { message: "Image is required!" })
+    imageURL: z
+        .instanceof(File)
+        .or(z.string()) // Handle cases where it might be a URL or string
+        .optional(),
 });
 
 export type RestaurantDataTypes = z.infer<typeof restaurantSchema>
