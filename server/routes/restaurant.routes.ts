c61f56ae008fd2d10621fbf51f45fe55f58c3c9b
Rakesh-99 <rakeshkumarparida424@gmail.com>
2024-12-07 22:30:55 +0530

    add more routes to restaurant

--- server/routes/restaurant.routes.ts
+++ server/routes/restaurant.routes.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 const restaturantRouters = express.Router();
-import { createRestaurant, deleteRestaurant, getRestaurantDetails, updateRestaurant } from '../controller/restaurant.controller';
+import { createRestaurant, deleteRestaurant, getRestaurant, getRestaurantDetails, getRestaurantOrder, searchRestaurant, updateOrderStaus, updateRestaurant } from '../controller/restaurant.controller';
 import uploadFile from '../middleware/multer.config';
 import { verifyUserByJwt } from '../middleware/verifyJwtToken';
 
@@ -12,6 +12,10 @@ restaturantRouters
     .get('/get-restaurant', verifyUserByJwt, getRestaurantDetails)
     .delete('/delete-restaurant', verifyUserByJwt, deleteRestaurant)
     .put('/update-restaurant', verifyUserByJwt, uploadFile.single("imageURL"), updateRestaurant)
+    .get('/restaurant-order', verifyUserByJwt, getRestaurantOrder)
+    .put('/order-status/:orderId', verifyUserByJwt, updateOrderStaus)
+    .get('/search/:searchParamsText', verifyUserByJwt, searchRestaurant)
+    .get('/view-restaurant/:id, verifyUserByJwt, getRestaurant')
 
 
 
