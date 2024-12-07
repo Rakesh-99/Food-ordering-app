a43000b6f23dfc15dad63956ef450c67df54dff3
Rakesh-99 <rakeshkumarparida424@gmail.com>
2024-12-07 22:40:47 +0530

    menu rendering dynamically

--- client/src/pages/admin/AvailableMenus.tsx
+++ client/src/pages/admin/AvailableMenus.tsx
@@ -1,36 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "../../components/ui/button"
 import { BsMenuButtonWide } from "react-icons/bs";
 import PopupModalMenu from "./PopupModalMenu";
-import pizzaImg from '../../assets/pizza.png'
 import EditMenuCard from "./EditMenuCard";
-import { MenuDetailTypes } from "../../constants/dataTypes";
+import useMenuStore from "../../store/menu-store/menuStore";
+
 
 const AvailableMenus = () => {
 
+    const [menuId, setMenuId] = useState<string>("");
 
     const [openMenu, setOpenMenu] = useState<boolean>(false);
     const [editOpenMenu, setEditOpenMenu] = useState<boolean>(false);
 
-
-    const menuDetails: MenuDetailTypes[] = [
-        {
-            imageFile: pizzaImg,
-            menu_name: 'Paneer Biriyani',
-            menu_price: 300,
-            menu_description: 'This is hydrabaadi speacial paneer biriyani you will ever have in your life.'
-        },
-        {
-            imageFile: pizzaImg,
-            menu_name: 'Paneer Biriyani',
-            menu_price: 300,
-            menu_description: 'This is hydrabaadi speacial paneer biriyani you will ever have in your life.'
-        }
-    ]
+    const { menus, getMenu } = useMenuStore();
 
 
 
+    const handleEditClick = (id: string) => {
+        setMenuId(id);
+        getMenu(id)
+        setEditOpenMenu(true);
+    };
 
+
+    console.log(menus);
 
 
 
@@ -56,49 +50,52 @@ const AvailableMenus = () => {
                 <div className="mt-16 g">
 
                     {/* Menu card */}
-
                     {
-                        menuDetails.map((val: MenuDetailTypes, idx: number) => {
-
+                        menus && menus.map((val: any) => {
 
                             return (
-
-                                <div className="md:flex-row flex-col md:items-center lg:items-center  items-start  flex  justify-around" key={idx} >
-
+                                <div className="md:flex-row flex-col md:items-center lg:items-center  items-start  flex  justify-around" key={val._id} >
 
                                     <div className="flex my-4 gap-3">
+
                                         {/* Image  */}
                                         <img
-                                            src={val.imageFile}
+                                            src={val.image}
                                             alt="menu image"
                                             className="w-28 border object-cover shadow-sm rounded-md px-2"
                                         />
 
+
                                         {/* Description  */}
                                         <div className="flex flex-col">
-                                            <h1 className="text-xl font-bold">{val.menu_name}</h1>
-                                            <p>{val.menu_description}</p>
+                                            <h1 className="text-xl font-bold">{val.name}</h1>
+                                            <p>{val.description}</p>
 
                                             {/* Price  */}
                                             <span className="flex items-center gap-1">
                                                 <h3 className="text-lg font-bold">Price :</h3>
-                                                <h3 className="text-blue-500 text-lg font-semibold">{val.menu_price}</h3>
+                                                <h3 className="text-blue-500 text-lg font-semibold">{val.price}</h3>
                                             </span>
                                         </div>
                                     </div>
 
                                     <div className="">
-                                        <Button type="button" onClick={() => setEditOpenMenu(true)} className="bg-blue-500 hover:bg-blue-600 w-28">Edit Menu</Button>
+                                        <Button type="button"
+                                            className="bg-blue-500 hover:bg-blue-600 w-28"
+                                            onClick={() => handleEditClick(val._id)}
+                                        >Edit Menu
+                                        </Button>
                                     </div>
 
                                     <EditMenuCard
-                                        menuDetails={val}
+                                        menuId={menuId}
                                         editOpenMenu={editOpenMenu}
                                         setEditOpenMenu={setEditOpenMenu} />
                                 </div>
                             )
                         })
                     }
+
                 </div>
             </div >
         </>
