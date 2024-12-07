e8c77a894ff601fd647fb6cd1464b02ba71487cd
Rakesh-99 <rakeshkumarparida424@gmail.com>
2024-12-07 22:20:04 +0530

    include routes to fetch single and multiple menu

--- server/routes/menu.routes.ts
+++ server/routes/menu.routes.ts
@@ -2,7 +2,7 @@ import express from 'express';
 const menuRouters = express.Router();
 import uploadFile from '../middleware/multer.config';
 import { verifyUserByJwt } from '../middleware/verifyJwtToken';
-import { createMenu, updateMenu } from '../controller/menu.controller';
+import { createMenu, getAllMenus, getSingleMenu, updateMenu } from '../controller/menu.controller';
 
 
 
@@ -11,5 +11,7 @@ import { createMenu, updateMenu } from '../controller/menu.controller';
 menuRouters
     .post('/create-menu', verifyUserByJwt, uploadFile.single("image"), createMenu)
     .put('/update-menu/:id', verifyUserByJwt, uploadFile.single("image"), updateMenu)
+    .get('/get-menu/:menuId', verifyUserByJwt, getSingleMenu)
+    .get('/get-menus', verifyUserByJwt, getAllMenus)
 
 export default menuRouters;
